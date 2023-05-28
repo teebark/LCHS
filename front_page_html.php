04bf72de1dba623165453ba6de8441e580d875cf
teebark <terry@teebark.com>
2023-05-28 11:57:59 -0400

    baseline pageHeader

---
+++ front_page_html.php
@@ -0,0 +1,114 @@
+<?php
+/** ---------------------------------------------------------------------
+ * themes/default/Front/front_page_html : Front page of site 
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2013 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * This source code is free and modifiable under the terms of 
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ *
+ * @package CollectiveAccess
+ * @subpackage Core
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
+ *
+ * ----------------------------------------------------------------------
+ */
+// Commented 10/5/2023
+//		print $this->render("Front/featured_set_slideshow_html.php");
+?>
+<body>
+	<!-- Commented 10/5/2023 -->
+    <span class='frontSlide'><a href="/Detail/objects/1079"><img src="http://collections.louisahistory.org/pawtucket/themes/LCHS/assets/pawtucket/graphics/2022.016.0011A.tif" width="700" height="540" alt="Schara Tzedeck Shoah survivors tribute book" /></a><div class='frontSlideCaption'><a href="/Detail/objects/7669">Schara Tzedeck Shoah survivors tribute book</a>, [2019]</div>
+ </span>
+	<span class='frontSlide'><a href="/Detail/objects/1079"><img src="http://collections.louisahistory.org/pawtucket/themes/LCHS/assets/pawtucket/graphics/Center Photo.jpg" width="700" height="540" alt="Schara Tzedeck Shoah survivors tribute book" /></a><div class='frontSlideCaption'><a href="/Detail/objects/7669">Schara Tzedeck Shoah survivors tribute book</a>, [2019]</div>
+ </span>
+            <span class='frontSlide'><a href="/Detail/objects/1079"><img src="http://collections.louisahistory.org/pawtucket/themes/LCHS/assets/pawtucket/graphics/1973.1.1_A.jpg" width="700" height="540" alt="Schara Tzedeck Shoah survivors tribute book" /></a><div class='frontSlideCaption'><a href="/Detail/objects/7669">Schara Tzedeck Shoah survivors tribute book</a>, [2019]</div>
+ </span>        
+<div class="row">
+    <div class="col-sm-12">
+<p class='homeDesc'><p>The Louisa County Historical Society welcomes you to explore our collections of historical documents, artifacts, photographs, and genealogical materials held at the Sargeant Museum of Louisa County History, located in Louisa, Virginia.</p>
+</p>
+			<p>Founded in 1966, the Louisa County Historical Society (LCHS) is a 501©3 non-profit organization with a mission to bring to light, preserve and promote the history of Louisa County. Our vision is to enlighten the present by illuminating the past in order to inspire the future. We share the stories of past Louisans through our museum exhibitions, educational programs, publications, research assistance, digital resources, and care of our growing collection of buildings, documents and artifacts. 
+
+</p>		<p>Our collections offer insights into the everyday life of Louisians as well as exceptional times in the county’s history, such as during both the Revolutionary and Civil Wars, and the Civil Rights Movement. Artifacts, documents, and oral histories range from Indigenous Native groups through the 21st century, and today continue to chronicle the story of present day Louisa County. Items held in the archive range from government and court data, documentation of family, school, and church life, information pertaining to agricultural and business development, vital records, and materials detailing the experiences of enslaved and freed African Americans.
+
+			<p>As our collection transitions to online access, staff and volunteers are continually adding, updating, and digitizing records at the Museum. We encourage the public to contact the Sargeant Museum to inquire about its holdings and to access non-digitized materials described in this system.
+</p>
+</p>		</div><!--end col-sm-12-->
+</div><!-- end col-->
+<!--div class='row'>	
+    <div class='featuredHome'><a href='/Museum/Index'><img src='https://collections.vhec.org/media/collectiveaccess/images/1/1/59453_ca_object_representations_media_1156_megawidepreview.jpg' width='500' height='300' alt='' /></a><a href='/Museum/Index'><div class='caption'>Museum</div></a></div>
+    <div class='featuredHome'><a href='/Archives/Index'><img src='https://collections.vhec.org/media/collectiveaccess/images/1/33638_ca_object_representations_media_152_megawidepreview.jpg' width='500' height='300' alt='' /></a><a href='/Archives/Index'><div class='caption'>Archives</div></a></div>				
+    <div class='featuredHome'><a href='/Library/Index'><img src='https://collections.vhec.org/media/collectiveaccess/images/4/93384_ca_object_representations_media_487_megawidepreview.jpg' width='500' height='300' alt='Rywka&#039;s diary : the writings of a Jewish girl from the Lodz Ghetto, found at Auschwitz in 1945 and published seventy years later' /></a><a href='/Library/Index'><div class='caption'>Library</div></a></div>	
+    <div class='featuredHome'><a href='/Testimony/Index'><img src='https://collections.vhec.org/media/collectiveaccess/images/2/5/84536_ca_object_representations_media_2527_megawidepreview.jpg' width='500' height='300' alt='' title='' /></a><a href='/Testimony/Index'><div class='caption'>Testimony</div></a></div>					
+</div>-->
+<div style="clear:both; height:1px;"><!-- empty --></div>
+    </div><!-- end pageArea --></div><!-- end col --></div><!-- end row --></div><!-- end container -->
+
+    <div id="footer">
+        <div class="list-inline pull-left">
+            <ul class="list-inline pull-left social">
+                <li><a href="https://www.facebook.com/Vancouver-Holocaust-Education-Centre-140874547755/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="https://twitter.com/VHECCollections" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            </ul>
+            <div class='pull-left footer-left'>
+                <p><b>P: 604-264-0499&nbsp;&nbsp;&nbsp;E: <a href='mailto:collections@vhec.org'>collections@vhec.org</a></b></p>
+                <p>50 – 950 West 41st Ave, Vancouver BC, V5Z 2N7 Canada</p>
+            </div>
+        </div>
+        
+        <div class="pull-right ca">
+            <div>powered by <a href="http://www.collectiveaccess.org">CollectiveAccess 2021</a></div>
+        </div>
+        
+        <div style="clear:both; height:1px;"><!-- empty --></div>
+    </div><!-- end footer -->
+            <div id="caMediaPanel"> 
+        <div id="caMediaPanelContentArea">
+        
+        </div>
+    </div>
+    <script type="text/javascript">
+        /*
+            Set up the "caMediaPanel" panel that will be triggered by links in object detail
+            Note that the actual <div>'s implementing the panel are located here in views/pageFormat/pageFooter.php
+        */
+        var caMediaPanel;
+        jQuery(document).ready(function() {
+            if (caUI.initPanel) {
+                caMediaPanel = caUI.initPanel({ 
+                    panelID: 'caMediaPanel',										/* DOM ID of the <div> enclosing the panel */
+                    panelContentID: 'caMediaPanelContentArea',		/* DOM ID of the content area <div> in the panel */
+                    exposeBackgroundColor: '#FFFFFF',						/* color (in hex notation) of background masking out page content; include the leading '#' in the color spec */
+                    exposeBackgroundOpacity: 0.7,							/* opacity of background color masking out page content; 1.0 is opaque */
+                    panelTransitionSpeed: 400, 									/* time it takes the panel to fade in/out in milliseconds */
+                    allowMobileSafariZooming: true,
+                    mobileSafariViewportTagID: '_msafari_viewport',
+                    closeButtonSelector: '.close'					/* anything with the CSS classname "close" will trigger the panel to close */
+                });
+            }
+        });
+        /*(function(e,d,b){var a=0;var f=null;var c={x:0,y:0};e("[data-toggle]").closest("li").on("mouseenter",function(g){if(f){f.removeClass("open")}d.clearTimeout(a);f=e(this);a=d.setTimeout(function(){f.addClass("open")},b)}).on("mousemove",function(g){if(Math.abs(c.x-g.ScreenX)>4||Math.abs(c.y-g.ScreenY)>4){c.x=g.ScreenX;c.y=g.ScreenY;return}if(f.hasClass("open")){return}d.clearTimeout(a);a=d.setTimeout(function(){f.addClass("open")},b)}).on("mouseleave",function(g){d.clearTimeout(a);f=e(this);a=d.setTimeout(function(){f.removeClass("open")},b)})})(jQuery,window,200);*/
+    </script>
+    </body>
+<?php
+		print $this->render("Front/gallery_set_links_html.php");
+?>
+		</div> <!--end col-sm-4-->	
+	</div><!-- end row -->
